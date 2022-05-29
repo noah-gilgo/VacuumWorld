2d8594f5cb68431644342dbde561caf32e478b57
noah-gilgo <74427747+noah-gilgo@users.noreply.github.com>
2022-05-29 12:05:11 -0400

    Properly documented both VacuumAgent.h and VacuumAgentMain.cpp. Changed the randomBool function to accept a parameter skewing the odds of the return value either being true or false. Changed the dirtyfy function to accept said parameter to allow user to change the probability of a square becoming dirty when the dirtyfy function is called. Removed redundant try-catch blocks.

--- VacuumWorld/VacuumAgent.h
+++ VacuumWorld/VacuumAgent.h
@@ -11,7 +11,7 @@
 	This is my first attempt at creating an intelligent agent. The agent is a direct implementation of the "vacuum world" agent referenced in Chapter 2 of 'Artificial
 	Intelligence, A Modern Approach (Fourth Edition) by Stuart Russell and Peter Norvig. It is a simple reflex agent operating in a fully observable, single-agent,
 	non-deterministic, episodic, static, discrete, and known task environment. The environment is an std::array containing 2 bools, which reflect if the current member of
-	the array is "clean".
+	the array is "clean". Since the agent is omniscient of its task environment, the terms 'percept' and 'task environment' are used interchangeably in documentation.
 
 	This agent is as bare-bones as they come, which is why I view it as a good way to get a hang of the basics of agents as well as it being an excellent foray into the
 	world of artificial intelligence.
@@ -20,17 +20,15 @@
 
 class VacuumAgent {
 public:
-	VacuumAgent() {}; // Constructor
-
+	VacuumAgent() {}; // Constructor for the VacuumAgent object. The C++ compiler gets very angry at me when I don't include the curly brackets.
+	
 	/*
-		Sets the percept for the VacuumAgent.
+		Randomly makes the members of the percept "dirty" (sets them to false).
 
-		@param vacuum_world An std::array of bools with length 2, the percept being viewed by the VacuumAgent.
-		@return void.
+		@param probability_of_dirty Sets the probability of a given member being set to "dirty" (1.0 has a 100% chance of making the member dirty, .5 has a 50% chance, etc.) 
 	*/
-
-	void dirtyfy() {
-		std::array<bool, 2> rand_world = { randomBool(), randomBool() };
+	void dirtyfy(double probability_of_dirty) {
+		std::array<bool, 2> rand_world = { randomBool(probability_of_dirty), randomBool(probability_of_dirty) };
 		if (vacuum_world[0] & (!rand_world[0])) {
 			vacuum_world[0] = false;
 		}
@@ -39,6 +37,11 @@ public:
 		}
 	}
 
+	/*
+		Sets the percept for the VacuumAgent. If the vacuum_world parameter is null, the function returns an error message.
+
+		@param vacuum_world An std::array of bools with length 2, the percept being viewed by the VacuumAgent.
+	*/
 	void set_percept(std::array<bool, 2> vacuum_world) {
 		try {
 			this->vacuum_world = vacuum_world;
@@ -46,10 +49,15 @@ public:
 			return;
 		}
 		catch (std::exception e) {
-			printf("ERROR: Please provide the agent with an environment to analyze.");
+			printf("ERROR: Task environment provided in set_percept function empty. Please initialize a full std::array<bool, 2>.");
 		}
 	}
 
+	/*
+		The agent function for VacuumAgent. If the current member is "dirty" (false), then the agent performs "suck", converting the member to clean. Otherwise, if the location
+		of the agent is set to 0 (corresponding to the left member of the percept), then the agent moves right. Otherwise, if the location is set to 1, the agent moves left.
+		If the percept of the agent has not been set to a task environment (vacuum_world == null), the function returns an error message.
+	*/
 	void agent_function() { // agent function.
 		try {
 			if (!vacuum_world[location]) { suck(); return; }
@@ -66,13 +74,8 @@ private:
 	std::array<bool, 2> vacuum_world; // Stores the percept of the agent.
 
 	void suck() { // sets the Boolean value in vacuum_world at index = location to true.
-		try {
-			vacuum_world[location] = true;
-			printf("%d - Suck\n", location);
-		}
-		catch (std::exception e) {
-			printf("ERROR: Please provide the agent with an environment to analyze.");
-		}
+		vacuum_world[location] = true;
+		printf("%d - Suck\n", location);
 	}
 
 	void left() { // sets the location to 0 (to point to the left member of vacuum_world)
@@ -85,7 +88,18 @@ private:
 		printf("%d - Right\n", location);
 	}
 
-	bool randomBool() {
-		return rand() > (RAND_MAX / 2);
+	/*
+		Generates and returns either true or false Boolean values, randomly. Parameter value should be between 1.0 and 0.0.
+
+		@param probability_of_true Double that represents the odds of false being returned. (ex. .75 has a 75% chance of returning false, .25 has a 25% chance, etc.)
+		@return Either true or false.
+	*/
+	bool randomBool(double probability_of_true) {
+		if ((probability_of_true < 0.0) ^ (probability_of_true > 1.0)) {
+			printf("Invalid parameter value for probability function. Value must be between 1.0 and 0.0.");
+		}
+		else {
+			return rand() > (RAND_MAX / (1.0 / probability_of_true));
+		}
 	}
 };
--- VacuumWorld/VacuumAgentMain.cpp
+++ VacuumWorld/VacuumAgentMain.cpp
@@ -3,15 +3,15 @@
 #include <random>
 #include "VacuumAgent.h"
 
-VacuumAgent agent;
-
 int main() {
-	std::array<bool, 2> vacuum_world{true, true};
+	VacuumAgent agent; // Initializes the agent
+	std::array<bool, 2> vacuum_world{true, true}; // Initializes the task environment
+	double probability_of_dirty = .1; // Sets the percent chance that the dirtyfy function will set a given member of the task environment to "dirty" when called.
 
-	agent.set_percept(vacuum_world);
+	agent.set_percept(vacuum_world); // Sets the agents percept on the task environment.
 
 	for (int i = 0; i < 100; i++) {
-		agent.dirtyfy();
-		agent.agent_function();
+		agent.dirtyfy(probability_of_dirty); // Either "dirtyfys" or leaves clean the members of the task environment depending on the value of probability_of_dirty.
+		agent.agent_function(); // Performs the agent function. The current location of the agent and the action it takes are printed to console.
 	}
 }
